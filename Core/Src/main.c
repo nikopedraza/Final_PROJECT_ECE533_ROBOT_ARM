92395684dee55aed4c28d564354476d93f17256e
Leonardo Niko Pedraza <pedrazaniko50@gmail.com>
2025-04-19 15:03:11 -0600

    Joystick code controlling motors working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,11 +1,21 @@
 #include "stm32l552xx.h"
 #include "stdio.h"
+#include "stdint.h"
 #include "i2c.h"
 
 
 #define SERVO_PIN = 5; // in GPIOC-
 #define SCALE_MOTOR 2 /// random value atm will configure
 #define RCC_CFGR_SW_HSI16 (2 << 0)
+// center and dead‐zone width
+#define CENTER   127
+#define DEAD     15
+
+// precompute the two thresholds
+enum {
+    HIGH_Z = CENTER + DEAD,   // 127 + 15 = 142
+    LOW_Z  = CENTER - DEAD    // 127 - 15 = 112
+};
 
 
 void init_sysclk();
@@ -84,6 +94,7 @@ int main(void) {
 	 initI2C();
 	 MCP23008_Init();
 	 init_TIM2();
+//	 init_TIM3();
 
 // DEMO CODE FOR TESTING THE DC MOTOR SETUP
 
@@ -149,6 +160,17 @@ void prev_step(StepIterator *it)
     it->current_index = (it->current_index + 7) % 8;
 }
 
+void reset_step(StepIterator *it){
+	it->current_index = 0;
+}
+
+//void dc_motors_off(Motor *motor){
+//
+//
+//
+//	MCP23008_WriteRegBlocking(motor->saddr,MCP23008_GPIO, 0x00);
+//}
+
 
 void updateMotorFromIterator(Motor *motor, StepIterator *iterator)
 {
@@ -158,7 +180,7 @@ void updateMotorFromIterator(Motor *motor, StepIterator *iterator)
     // Update the Motor struct fields accordingly.
 
     // Update the Motor struct fields accordingly.
-     motor->data |= (motor->pos & step);
+     motor->data = (motor->pos & step);
 
 }
 
@@ -193,7 +215,7 @@ void init_TIM2(void)
     TIM2->PSC = 15;
 
     // 3. Set the auto-reload register (ARR) for a 1 kHz update rate.
-    TIM2->ARR = 4999; // 1M/1000 = 1khz timer
+    TIM2->ARR = 200; // 1M/1000 = 1khz timer
 
     // 4. Reset the counter.
     TIM2->CNT = 0;
@@ -222,7 +244,7 @@ void init_TIM3(void)
 
     // 2. Set prescaler and ARR (same settings as TIM2 for a 1 kHz rate).
     TIM3->PSC = 15;   // Timer clock = 16MHz/16 = 1MHz.
-    TIM3->ARR = 999;  // Counter counts 0..999 -> update every 1 ms.
+    TIM3->ARR = 100000;  // Counter counts 0..999 -> update every 1 ms.
 
     // 3. Clear the counter.
     TIM3->CNT = 0;
@@ -272,18 +294,95 @@ void TIM2_IRQHandler() {
 	// Clear the update interrupt flag
 	TIM2->SR &= ~TIM_SR_UIF;
 
+
+
+	uint8_t data1[2];
+	uint8_t data2[2];
+	uint8_t adc_x1,adc_y1,adc_x2,adc_y2;
+
+
+
+	MCP23008_ReadRegBlocking(JOYSTICK_L_SADDR, JOYSTICK_8BIT_REG,2,data1);
+	MCP23008_ReadRegBlocking(JOYSTICK_R_SADDR, JOYSTICK_8BIT_REG,2,data2);
+
+
+	adc_x1 = data1[0]; //base direction value
+	adc_y1 = data1[1]; //lower motor direction value
+	adc_x2 = data2[0]; //upper motor direction value
+	adc_y2 = data2[1]; //servo value
+
+
+
+
+
+
+    // X1
+    if (adc_x1 > HIGH_Z) {
+        // above dead‐zone
+    	next_step(&base_iter);
+    }
+    else if (adc_x1 < LOW_Z) {
+        // below dead‐zone
+    	prev_step(&base_iter);
+
+    }
+    else {
+        // inside dead‐zon
+    	// stays on current index
+//    	dc_motor_off(base);
+    }
+
+    // Y1
+    if (adc_y1 > HIGH_Z) {
+        // above dead‐zone
+    	next_step(&lower_iter);
+    }
+    else if (adc_y1 < LOW_Z) {
+        // below dead‐zone
+    	prev_step(&lower_iter);
+    }
+    else {
+        // inside dead‐zone
+    }
+
+    // X2
+    if (adc_x2 > HIGH_Z) {
+        // above dead‐zone
+    	next_step(&upper_iter);
+
+    }
+    else if (adc_x2 < LOW_Z) {
+        // below dead‐zone
+    	prev_step(&upper_iter);
+
+    }
+    else {
+        // inside dead‐zone
+    }
+
+    // Y2
+    if (adc_y2 > HIGH_Z) {
+        // above dead‐zone
+    }
+    else if (adc_y2 < LOW_Z) {
+        // below dead‐zone
+    }
+    else {
+        // inside dead‐zone
+    }
+
+
+	updateMotorFromIterator(&upper,&upper_iter);
+
+
+	updateMotorFromIterator(&lower,&lower_iter);
+
+	updateMotorFromIterator(&base,&base_iter);
+
+
 	// any thing polling ?
-		processExpanderChange();
+	processExpanderChange();
 
-
-		next_step(&upper_iter);
-		updateMotorFromIterator(&upper,&upper_iter);
-
-		next_step(&lower_iter);
-		updateMotorFromIterator(&lower,&lower_iter);
-
-		next_step(&base_iter);
-		updateMotorFromIterator(&base,&base_iter);
 //	next_step(&base_iter);
 //
 //	updateMotorFromIterator(&base, &base_iter);
@@ -291,9 +390,19 @@ void TIM2_IRQHandler() {
 void TIM3_IRQHandler() {
 	// Clear the update interrupt flag
 	TIM3->SR &= ~TIM_SR_UIF;
-	next_step(&lower_iter);
 
-	updateMotorFromIterator(&lower, &lower_iter);
+	uint8_t data[4];
+	uint8_t adc_x;
+	uint8_t adc_y;
+
+
+	MCP23008_ReadRegBlocking(JOYSTICK_L_SADDR, JOYSTICK_8BIT_REG,2,data);
+
+	adc_x = data[0];
+	adc_y = data[1];
+
+
+
 }
 void TIM4_IRQHandler() {
 	// Clear the update interrupt flag
